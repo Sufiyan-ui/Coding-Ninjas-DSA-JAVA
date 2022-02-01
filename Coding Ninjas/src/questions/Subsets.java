abc11a218fa43eb7aaf73c6b75c8e137824f307d
Mohd Sufiyan <msufiyan576@gmail.com>
2022-02-01 14:19:21 +0530

    optimize solution

--- Coding Ninjas/src/questions/Subsets.java
+++ Coding Ninjas/src/questions/Subsets.java
@@ -2,27 +2,44 @@ package questions;
 
 //	print subsets of array
 public class Subsets {
-	public static void printSubsets(int[] ar, int[] output, int sI, int eI) {
-		if(sI >= eI) {
-			for(int i:output) {
-				System.out.print(i);
-			}
-			System.out.println();
-			return;
-		}
-		printSubsets(ar, output, sI+1, eI);
-		output = new int[ar.length-sI];
-		int j = 0;
-		for(int i=sI; i<ar.length; i++) {
-			output[j] = ar[i];
-			i++;
-		}
-		printSubsets(ar, output, sI+1, eI);
+	public static int[][] subsets(int input[],int si) {
+        if(si>=input.length)
+        {
+            int ans[][]=new int[1][0];
+            return ans;
+        }
+         
+        int[][] smallans=subsets(input,si+1);
+        int[][] ans=new int[smallans.length*2][];
+        int k=0;
+        for(int i=0;i<smallans.length;i++){
+            ans[i]=new int[smallans[i].length];
+            for(int j=0;j<smallans[i].length;j++){
+                ans[i][j]=smallans[i][j];
+            }
+            k++;
+        }
+         
+        for(int i=0;i<smallans.length;i++){
+          ans[k+i]=new int[smallans[i].length+1];
+          ans[k+i][0]=input[si];
+          for(int j=1;j<=smallans[i].length;j++){
+            ans[i+k][j]=smallans[i][j-1];
+            }
+        }
+        return ans;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] ar = {1,2,3};
-		printSubsets(ar, new int[] {}, 0, ar.length-1);
+		int[] ar = {1,2,3,4,5};
+		int[][] array2 = subsets(ar, 0);
+		
+		for(int[] a: array2) {
+			for(int it:a) {
+				System.out.print(it+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
